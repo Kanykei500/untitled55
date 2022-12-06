438c7a75a1b59c90078693d30bcb9953ec767448
Kanykei500 <askarbekovakanykei120@gmail.com>
2022-12-06 18:02:08 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Set<Integer> set=new HashSet<>(Arrays.asList(1,2,3));
+        Set<Integer>set1=new HashSet<>(Arrays.asList(0,1,2));
+        System.out.println(set);
+        System.out.println(set1);
+        System.out.println(symmetricDifference(set,set1));
+    }
+    public static Set<Integer> symmetricDifference (Set<Integer> set, Set<Integer> set1){
+        Set<Integer>set2=new HashSet<>(set);
+        set2.addAll(set1);
+        Set<Integer>set3=new HashSet<>(set);
+        set3.retainAll(set1);
+        set2.removeAll(set3);
+        return set2;
     }
 }
